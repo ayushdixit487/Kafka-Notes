f14ccd7a515bd9e5d84205fd5a478cf85084d80f
Ayush Dixit <ayushdixit661@gmail.com>
2020-07-02 22:22:54 +0530

    Create KafkaProducerMain.java

---
+++ Kafka Producer/KafkaProducerMain.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaProducerMain {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(KafkaProducerMain.class);
+
+    public static void main(String[] args) throws Exception {
+        LOGGER.info("Running kafka producer...");
+        CustomKafkaProducer customKafkaProducer = new CustomKafkaProducer();
+        customKafkaProducer.produce("sample-topic","1", "This is a sample message");
+        LOGGER.info("Running kafka producer completed!! ");
+    }
